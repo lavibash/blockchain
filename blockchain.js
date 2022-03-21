d2dfd5cd66228ee327138e6be1d11abfcf02418f
LaviAvigad <92415264+lavibash@users.noreply.github.com>
2022-03-20 19:44:09 -0700

    Update blockchain.js
    forged blockchain in javascript!!

--- blockchain.js
+++ blockchain.js
@@ -1,46 +1,41 @@
-const SHA256 = require('crypto-js/sha256')
-const main = require('crypto')
-
+const SHA256 = require('crypto-js/sha256');
+//the cryptoblock of the chain
 class Block{
-    constructor(timestamp, data, previousHash = '') {
-        this.previousHash = previousHash;
+    constructor(index, timestamp, data, precedingHash=" ") {
+        this.index = index;
         this.timestamp = timestamp;
         this.data = data;
-        this.hash = this.hash;
+        this.precedingHash = precedingHash;
         this.nonce = 0;
+        this.hash = this.computeHash();
+    }
+    computeHash(){
+        return SHA256(this.index + this.precedingHash + this.timestamp + JSON.stringify(this.data)).toString(); //parameters
+    }
+}
+class Blockchain{
+    constructor() {
+        this.blockchain = [this.startGenesisBlock()]; //the array of blocks
+    }
+    startGenesisBlock(){ //cearting the block using .startGenesisBlock() method
+        return new Block(0, '03/15/2022', "Intial Block in the Chain", "0"); //blocks are created with genesis which start with an index of 0
+    }
+    obtainLatestBlock = () => {
+        return this.blockchain[this.blockchain.length - 1]; //needs to reference the preceding block
     }
     addNewBlock(newBlock){
         newBlock.precedingHash = this.obtainLatestBlock().hash; //using the method to retrieve the last block so the chain can reference it
         newBlock.hash = newBlock.computeHash();
         this.blockchain.push(newBlock);
     }
-mineBlock(difficulty) {
-     // Keep changing the nonce until the hash of our block starts with enough zero's.
-        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
-            this.nonce++;
-            this.hash = this.calculateHash();
-        }
+}
+//testing the blockchain
+let catcoin = new Blockchain();
+catcoin.addNewBlock(new Block(1, "03/15/2022", {sender: "Lavi Avigad", recipient: "Lidor Avigad", quantity: 50}));
+catcoin.addNewBlock(new Block(2, "3/15/2022", {sender: "Lavi Avigad", recipient: "Lavi Avigad", quantity: 100}));
+console.log(JSON.stringify(catcoin, null, 4));
 
-        console.log("BLOCK MINED: " + this.hash);
-    }
-    obtainLatestBlock = () => {
-        return this.blockchain[this.blockchain.length - 1]; //needs to reference the preceding block
-    }
-        computeHash(){
-            return SHA256(this.index + this.precedingHash + this.timestamp + JSON.stringify(this.data)).toString(); //parameters
-        }
-blockchain = () => {
-        this.chain = [this.creatGenesisBlock()];
-        this.difficulty = 3;
-}
-    addBlock = (newBlock) => {
-        newBlock = newBlock.previousHash;
-        newBlock= this.obtainLatestBlock().hash;
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
-        return newBlock
-    }
-}
+
 checkChainValidity = () => {
     for(let i = 1; i < this.blockchain.length; i++){
         const currentBlock = this.blockchain[i];
@@ -54,7 +49,3 @@ checkChainValidity = () => {
     }
     return true;
 }
-let catcoin = new Block();
-catcoin.addNewBlock(new Block(1, "03/15/2022", {sender: "Lavi Avigad", recipient: "Lidor Avigad", quantity: 50}));
-catcoin.addNewBlock(new Block(2, "3/15/2022", {sender: "Lavi Avigad", recipient: "Lavi Avigad", quantity: 100}));
-console.log(JSON.stringify(catcoin, null, 4));
